44d78fdb0e7983f9d348edefccac1dcad05ea20c
antoinecarter <isaiah.carter136@gmail.com>
2022-02-17 10:56:50 -0400

    Formatted

--- maximum/New folder/model/Model.php
+++ maximum/New folder/model/Model.php
@@ -1,17 +1,16 @@
 <?php
 include_once("model/Model.php");
-class Model {
-public function getlogin()
+class Model
 {
-// here goes some hardcoded values to simulate the database
-if(isset($_REQUEST['username']) && isset($_REQUEST['password'])){
-if($_REQUEST['username']=='admin' && $_REQUEST['password']=='admin'){
-return 'login';
+    public function getlogin()
+    {
+        // here goes some hardcoded values to simulate the database
+        if (isset($_REQUEST['username']) && isset($_REQUEST['password'])) {
+            if ($_REQUEST['username'] == 'admin' && $_REQUEST['password'] == 'admin') {
+                return 'login';
+            } else {
+                return 'invalid user';
+            }
+        }
+    }
 }
-else{
-return 'invalid user';
-}
-}
-}
-}
-?>
