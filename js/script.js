ea00679be6ec4adf79651d288e4662c3acab11e9
Blase7 <56705087+Blase7@users.noreply.github.com>
2022-02-26 15:44:26 -0400

    *

--- js/script.js
+++ js/script.js
@@ -7,3 +7,13 @@ function closeNav() {
     document.getElementById("mySidebar").style.width = "0";
     document.getElementById("main").style.marginLeft = "0";
 }
+
+window.addEventListener("DOMContentLoaded", function() {
+    let boxes = document.querySelectorAll(".box");
+
+    Array.from(boxes, function(box) {
+        box.addEventListener("click", function() {
+            alert(this.classList[1]);
+        });
+    });
+});
