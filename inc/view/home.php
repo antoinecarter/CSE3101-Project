963454ddc4f43b2d80101733970bdd37c663ff3f
tiatonic <caseylewis201@gmail.com>
2022-02-25 22:21:04 -0400

    added a new line

--- inc/view/home.php
+++ inc/view/home.php
@@ -1,6 +1,22 @@
-<div>
-    <?php
-    require __DIR__."/inc.php";
-        echo $_SESSION['id'];
-    ?>
-</div>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./css/style.scss" type="text/css">
+    </head>
+    <div>
+        <?php
+        require __DIR__."/inc.php";
+            echo $_SESSION['id'];
+        ?>
+    </div>
+<body>
+    <div class="home">
+
+    </div>
+
+
+</body>
+  
+</html>
