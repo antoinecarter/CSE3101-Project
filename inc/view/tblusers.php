96b979d6cd9327319b9340bfb6ccf0fea159314e
antoinecarter <isaiah.carter136@gmail.com>
2022-02-27 17:58:34 -0400

    *

--- inc/view/tblusers.php
+++ inc/view/tblusers.php
@@ -15,14 +15,14 @@ $num_rows = $statement->rowCount();
     print_r($url_components['path']); ?>
     <h5>Home/User Accounts</h5>
 </div>
-<div>
+<div class="top">
     <h2>Listing of User Accounts</h2>
 </div>
 <div>
     <a href="./Users/Registration"><button>Add New</button></a>
 </div>
-<div>
-    <table>
+<div class="tble">
+    <table class="users">
         <thead>
             <th>Edit</th>
             <th>Username</th>
@@ -52,58 +52,6 @@ $num_rows = $statement->rowCount();
         </tbody>
     </table>
 </div>
-
-
-    <?php
-    include __DIR__ . "/header.php";
-    $usercontroller = new UsersController();
-    $statement = $usercontroller->viewusers();
-    $num_rows = $statement->rowCount();
-    ?>
-    <div class="breadcrumb">
-        <?php echo $num_rows; ?>
-        <h5>Home/User Accounts</h5>
-    </div>
-    <div class="tope">
-        <h2>Listing of User Accounts</h2>
-    </div>
-    <div>
-        <?php echo $_SESSION['id']; ?>
-        <a href="./Users/Registration"><button>Add New</button></a>
-    </div>
-    <div class="tble">
-        <table class="users">
-            <thead>
-                <th>Edit</th>
-                <th>Username</th>
-                <th>Fullname</th>
-                <th>Email</th>
-                <?php if($_SESSION['role'] == 'ADMIN'){ echo '<th>Role</th>'; } ?>
-                <th>Effective From</th>
-                <th>Effective To</th>
-            </thead>
-            <tbody>
-                <?php
-                    if($num_rows == 0){
-                        echo '<tr><td colspan="7" style="text-align: center; font-family: Lato, sans-serif; font-size: 20px; font-weight: bolder">--No Data Found--</td></tr>';
-                    }
-                    while($row = $statement->fetch(PDO::FETCH_ASSOC)){
-                ?>
-                    <tr>
-                        <td><a href="Users/Registration/Edit/<?php echo $row['id'];?>"><img alt= "" style="width:30px; height:30px" src="include/edit.png"></a></td>
-                        <td><?php echo $row['username']; ?></td>
-                        <td><?php echo $row['last_name'] .','. $row['first_name']; ?></td>
-                        <td><?php echo $row['email']; ?></td>
-                        <?php if($_SESSION['role'] == 'ADMIN'){?> <td><?php echo $row['role']; ?></td> <?php } ?>
-                        <td><?php echo date_format(date_create($row['start_date']), "d-M-Y"); ?></td>
-                        <td><?php if(isset($row['end_date'])){echo date_format(date_create($row['end_date']), "d-M-Y");}else{ echo '-';} ?></td>
-                    </tr>
-                <?php } ?>
-            </tbody>
-        </table>
-    </div>
-
     <?php
     include __DIR__ . "/footer.php";
     ?>
-</html>
