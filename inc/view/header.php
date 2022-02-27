73539373983fb83f267fa2e0aa17552bae4e7323
glamourousgecko <glamourousgecko@protonmail.com>
2022-02-27 13:50:49 -0400

    Update branch for homepage design

--- inc/view/header.php
+++ inc/view/header.php
@@ -1,56 +1,21 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-  <meta charset="UTF-8">
+    <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./css/style.scss" type="text/css">
-  </head>
-  
-    <header>
-      <nav class="side-top-nav">
-      </head>
-
-        <div id="mySidebar" class="sidebar">
-          <a href="javascript:void(0)" class="closebtn" onclick="closeNav()">×</a>
-          <a href="#">About</a>
-          <a href="#">Services</a>
-          <a href="#">Clients</a>
-          <a href="#">Contact</a>
-        </div>
-
-        <div id="main">
-          <button class="openbtn" onclick="openNav()">☰</button>  
-
-        </div>
-        <script src="./js/script.js"></script>
-        <div></div>
-
-
-        <a class="cur_user"> HMRS  </a>
-
-        <div class="topnav_button">
-          <a href="./home">home</a>
-          <a href="#">about</a>
-
-          <div class="dropmenu">
-          <button class="dropbb"><?php require __DIR__."/inc.php"; echo $_SESSION['username'];?>  ▼</button>
-        <div class="dropmenu-content">
-          <a href="#">Profile</a>
-          <a href="action=logout"> Logout</a>
-        </div> 
-
-
-      </nav>
-      <script src="./js/script.js"></script>
-
-  </div>
-</div>
-      
-    </header>
-    <body>
+    <link rel="stylesheet" href="/css/style.scss" type="text/css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  </head> 
+  <body>
+    <nav class="navbar navbar-light bg-info">
+      <div class="container">
+        <a class="navbar-brand" href="#">
+          <h1 class="brand">HRMS</h1>
+        </a>
+      </div>
+    </nav>  
   </body>
-
 </html>
 
   
