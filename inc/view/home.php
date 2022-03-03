7e4bb05503fb8c0948c71d9575cc4a94d8ed9a1d
Blase7 <56705087+Blase7@users.noreply.github.com>
2022-03-02 22:28:56 -0400

    *

--- inc/view/home.php
+++ inc/view/home.php
@@ -7,15 +7,17 @@ include __DIR__."/header.php";
     <h1>Human Resources Dashboard</h1>
 
     <div class="wrapper">
-        <div class="box a">Departments</div>
-        <div class="box b">Organizational Structure</div>
-        <div class="box c">Units</div>
-        <div class="box d">Positions</div>
-        <div class="box e">Shifts</div>
-        <div class="box f">Employee</div>
-        <div class="box f">Leave</div>
-        <div class="box f">References</div>
-        <div class="box f">Timeclock</div>
+
+        <a class="box-e" href ="./Departments">Departments <p>text here</p></a>
+        <a class="box-e" href ="./Absence">Absence</a>
+        <a class="box-e" href ="./Units">Units</a>
+        <a class="box-e" href ="./Positions">Positions</a>
+        <a class="box-e" href ="./Shifts">Shifts</a>
+        <a class="box-e" href ="./Employees">Employee</a>
+        <a class="box-e" href ="./Leavetrack">Leave</a>
+        <a class="box-e" href ="./References">References</a>
+        <a class="box-e" href ="./Timeclock">Timeclock</a>
+        <a class="box-e" href ="./Salary">Salary</a>
 
     </div>
 </div>
