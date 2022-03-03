06245ef33ccde2cd65a1d978b2894c2849c75a47
Blase7 <56705087+Blase7@users.noreply.github.com>
2022-03-03 12:08:46 -0400

    *

--- inc/view/tblreferences.php
+++ inc/view/tblreferences.php
@@ -12,12 +12,12 @@ $num_rows = $statement->rowCount();
     $url .= $_SERVER['REQUEST_URI'];
     $url_components = parse_url($url);
     parse_str($url_components['path'], $params);?>
-    <h5>Positions</h5>
+    <h5>References</h5>
 </div>
 <div class = "usrtb">
-        <h2>Listing of Positions
-        <a href="./Positions/Registration"><button>Add Pos.</button></a></h2>
-        <a style= "margin-left: 7px;"> Num Of Positions: <?php echo $num_rows; ?></a>
+        <h2>Listing of References
+        <a href="./References/Registration"><button>Add Pos.</button></a></h2>
+        <a style= "margin-left: 7px;"> Num Of References: <?php echo $num_rows; ?></a>
         <div class="tblfx">
         <table>
             <thead>
@@ -39,7 +39,7 @@ $num_rows = $statement->rowCount();
                     while($row = $statement->fetch(PDO::FETCH_ASSOC)){
                 ?>
                 <tr>
-                    <td><a href="./Positions/Registration/Edit?id=<?php echo $row['id'];?>"><img style="width:30px; height:30px" src="./inc/view/include/edit.png"></a></td>
+                    <td><a href="./References/Registration/Edit?id=<?php echo $row['id'];?>"><img style="width:30px; height:30px" src="./inc/view/include/edit.png"></a></td>
                     <td><?php echo $row['org_id']; ?></td>
                     <td><?php echo $row['table_name']; ?></td>
                     <td><?php echo $row['table_desc']; ?></td>
